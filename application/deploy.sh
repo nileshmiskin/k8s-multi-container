98882cad0bdd0dedd0431e4316d963d57f352958
Nilesh Miskin <63668366+nmiskininnovect@users.noreply.github.com>
2020-05-09 21:12:49 +0530

    [NM] Add deployment scripts to deploy to GKE

---
+++ application/deploy.sh
@@ -0,0 +1,19 @@
+# build images
+docker build -t nileshmiskin/multi-client:latest -t nileshmiskin/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nileshmiskin/multi-server:latest -t nileshmiskin/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nileshmiskin/multi-worker:latest -t nileshmiskin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# already logged into docker hub, so just push
+docker push nileshmiskin/multi-client:latest
+docker push nileshmiskin/multi-server:latest
+docker push nileshmiskin/multi-worker:latest
+
+docker push nileshmiskin/multi-client:$SHA
+docker push nileshmiskin/multi-server:$SHA
+docker push nileshmiskin/multi-worker:$SHA
+
+# apply k8s 
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=nileshmiskin/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nileshmiskin/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nileshmiskin/multi-worker:$SHA
