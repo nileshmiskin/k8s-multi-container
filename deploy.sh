936c3595641c6cf3f817c38ec8b116645a991c39
Nilesh Miskin <63668366+nmiskininnovect@users.noreply.github.com>
2020-05-11 22:23:11 +0530

    [NM] Add more logging to scripts

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
 # build images
+echo "BUILDING IMAGES"
 docker build -t nileshmiskin/multi-client:latest -t nileshmiskin/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t nileshmiskin/multi-server:latest -t nileshmiskin/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t nileshmiskin/multi-worker:latest -t nileshmiskin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # already logged into docker hub, so just push
+echo "CREATING LATEST TAG"
 docker push nileshmiskin/multi-client:latest
 docker push nileshmiskin/multi-server:latest
 docker push nileshmiskin/multi-worker:latest
 
+echo "CREATING $SHA TAG"
 docker push nileshmiskin/multi-client:$SHA
 docker push nileshmiskin/multi-server:$SHA
 docker push nileshmiskin/multi-worker:$SHA
 
+echo "APPLY K8S"
 # apply k8s 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=nileshmiskin/multi-server:$SHA
